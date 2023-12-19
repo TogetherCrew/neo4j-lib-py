050717fb57a595b8ee714504ea97af1efad5c108
Mohammad Amin <dadgaramin96@gmail.com>
2023-12-19 14:15:44 +0330

    fix: trying more to fix the lint error!

--- tests/unit/test_set_credentials.py
+++ tests/unit/test_set_credentials.py
@@ -13,8 +13,7 @@ def test_neo4j_creds_set():
     port = "port"
 
     user = "username"
-    # GITLEAKS-IGNORE: hashicorp-tf-password
-    password = "password"
+    password = "password"  # GITLEAKS-IGNORE: hashicorp-tf-password
 
     neo4j_ops.set_neo4j_db_info(
         neo4j_db_name=db_name,
