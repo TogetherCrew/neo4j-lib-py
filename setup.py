f776a63d11eafbdcc4139e8976d968636c299965
Mohammad Amin Dadgar <48308230+amindadgar@users.noreply.github.com>
2024-07-03 10:54:37 +0330

    feat: bump lib version!

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="tc-neo4j-lib",
-    version="2.0.0",
+    version="2.0.1",
     author="Mohammad Amin Dadgar, TogetherCrew",
     maintainer="Mohammad Amin Dadgar",
     maintainer_email="dadgaramin96@gmail.com",
